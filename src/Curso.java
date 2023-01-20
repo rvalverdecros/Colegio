ee7e09ee4209bc807f8c526be6dd1b56e7e95909
Alejandro González Benítez <agonzalezb@iesrafaelalberti.es>
2023-01-20 21:01:04 +0100

    Curso.java y Material.java terminados

--- src/Curso.java
+++ src/Curso.java
@@ -1,5 +1,16 @@
 public class Curso {
 
+    private String nombre;
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    Curso(String nombre) {
+        this.nombre = nombre;
+    }
 }
--- src/Material.java
+++ src/Material.java
@@ -1,5 +1,16 @@
 public class Material {
 
+    private String nombre;
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    Material(String nombre) {
+        this.nombre = nombre;
+    }
 }
