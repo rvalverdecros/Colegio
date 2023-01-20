0ed0c8761f152a6c51f4068eae89b97a6ff6e0c3
Rafa <rvalverde@iesrafaelalberti.es>
2023-01-20 20:24:47 +0100

    Union Juanma y Rafa

--- src/Direccion.java
+++ src/Direccion.java
@@ -1,10 +1,21 @@
-public class Direccion {
+public class Direccion implements JuntaDirectiva {
     private int npersonas;
     private boolean decision;
+    private boolean juntadecision;
 
     public Direccion(int personas){
         this.npersonas = personas;
         decision = true;
+    }
+
+    @Override
+    public void afirmacion(){
+        juntadecision = true;
+    }
+
+    @Override
+    public void negacion(){
+        juntadecision = false;
     }
 
     public void denegarDecision(){
