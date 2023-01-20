af5e588bc96e864b98830091726ae7b8f686d529
rafa <rvalcro022@g.educaand.es>
2023-01-21 00:47:25 +0100

    Añadir Menu (Faltan 2 opciones)

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,16 @@ public class Main {
         System.out.println("2. Creación de curso");
         System.out.println("3. Creación de profesor");
         System.out.println("4. Creación de alumno");
-        System.out.println("5. ");
+        System.out.println("5. Modificacion de aula");
+        System.out.println("6. Modificacion de curso");
+        System.out.println("7. Modificacion de profesor");
+        System.out.println("8. Modificacion de alumno");
+        System.out.println("9. Borrar un aula");
+        System.out.println("10. Borrar un curso");
+        System.out.println("11. Borrar un profesor");
+        System.out.println("12. Borrar un alumno");
+        System.out.println("13. Mostrar todo");
+        System.out.println("14. Cerrar Sesión");
     }
 
     public static void main(String[] args) {
